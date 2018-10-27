fd5d5ef1888a16d0b5b892c199b89748d5bcd465
mmani9199 <manikantkumar771@gmail.com>
2018-10-27 13:28:21 +0530

    Lex program to capitalize the comments

---
+++ text.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+    //my name is manikant kumar
+    for(int i = 0; i < 10; i++)
+    {
+        /*
+        asasasa
+        */
+        cout<<i<<"\n";
+    }
+    return 0;
+}
+
